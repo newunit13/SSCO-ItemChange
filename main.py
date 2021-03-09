f69bf4e61377cbddd02d7223ca3a3c649540fc8c
newunit13 <newunit13@gmail.com>
2021-03-09 00:40:26 -0500

    A lot of changes

--- main.py
+++ main.py
@@ -1,43 +1,109 @@
-import pandas as pd
 from fpdf import FPDF
+import pandas as pd
 
+WIDTH = 210
+HEIGHT = 297
 
-
-#create PDF object
 class PDF(FPDF):
-    pass
+    def create_header(self, date, cust_acct, cust_name, cust_street, cust_city, cust_state, cust_zip):
+        self.set_font('Arial', '', 11)
+        self.image('assets/logo.png', (WIDTH/3)*2-20, 20, WIDTH/3)
 
-pdf = PDF(format='Letter')
-pdf.add_page()
+        # Customer Address
+        self.ln(10)
+        self.cell(10)
+        self.multi_cell(90, 6, f"""
+Customer ID: {cust_acct}                             {date}
+{cust_name}
+{cust_street}
+{cust_city}, {cust_state} {cust_zip}
+""", 0)
+        self.ln(10)
+
+    # table of items
+    def create_table_header(self, widths, columns):
+        """
+        Pass in a list a values to create headers
+        """
+        self.set_font('Arial', 'B', 11)
+        self.set_fill_color(0,150,0)
+        self.set_text_color(255)
+
+        for width, header in zip(widths, columns):
+            self.cell(width, 5, header, 1,0,'C',True)
+
+        self.set_text_color(0)
+        self.set_fill_color(255,255,255)
+        self.ln()
 
 
+    def create_table_row(self, widths, row):
+        self.set_font_size(9)
+        for width, datum in zip(widths, row):
+            self.cell(width, 5, self.format_data(datum), 1,0,'',True)
+        self.ln()
 
+    def create_table_footer(self):
+        self.ln(20)
+        self.set_font('Arial', '', 11)
+        # Signature Area
+        self.cell(5)
+        self.cell(65, 5, 'Name', 'T', 0, 'C')
+        self.cell(7)
+        self.cell(65, 5, 'Signature', 'T', 0, 'C')
+        self.cell(7)
+        self.cell(30, 5, 'Date', 'T' ,0, 'C')
 
-df = pd.read_excel("data/data.xlsx")
+    def format_data(self, datum):
 
+        if type(datum) == str:
+            if len(datum) > 30:
+                datum = datum[:30] + '...'
+        elif type(datum) == float:
+            datum = f'${datum:0.02f}'
+            if datum == '$nan':
+                datum = ''
+        elif type(datum) == pd._libs.tslibs.timestamps.Timestamp:
+            datum = f'{datum.date()}'
+        
+        return datum
+
+df = pd.read_excel("data/data.xlsx", converters={'Acct': str})
 grouped_df = df.groupby(['Acct'])
 
-for customer_df in grouped_df:
+for cust_acct, data in grouped_df:
 
-    items_df = customer_df[1]
+    pdf = PDF()
+    pdf.add_page()
 
-    if len(items_df) == 1:
-        record = items_df[['Name', 'Item #', 'Item Name', 'Customer Part #', 'Unit', 'New Price']]
-        customer = record['Name'].item()
-        item_no = record['Item #'].item()
-        item_name = record['Item Name'].item().strip()
-        cust_item_no = record['Customer Part #']
-        item_uom = record['Unit']
-        price = float(record['New Price'].item())
-        
-        print(f"Customer: {customer}\n\tItem: {item_no}\tDescription: {item_name}\tNew Price: {price:02f}")
-    else:
-        print(f"\nCustomer: {customer_df[1]['Name'].iloc[0]}")
-        for record in customer_df[1][['Name', 'Item #', 'Item Name', 'Customer Part #', 'Unit', 'New Price']].iterrows():
-            item_no = record[1]['Item #']
-            item_name = record[1]['Item Name'].strip()
-            cust_item_no = record[1]['Customer Part #']
-            item_uom = record[1]['Unit']
-            price = float(record[1]['New Price'])
-            print(f"\tItem: {item_no}\tDescription: {item_name}\tNew Price: {price:02f}")
-        print()
+    acct    = cust_acct
+    name    = data.iloc[0]["Name"]
+    street  = data.iloc[0]["Customer Street"]
+    city    = data.iloc[0]["Customer City"]
+    state   = data.iloc[0]["Customer State"]
+    zipcode = data.iloc[0]["Customer Zip"]
+    date    = '01/01/1900'
+
+    pdf.create_header(date, acct, name, street, city, state, zipcode)
+
+    item_df = data[["Item #", 'Customer Part #', 'Item Name', 'Unit', 'New Price', 'From Date']]
+
+    widths = [28, 32, 60, 12, 30, 30]
+    pdf.create_table_header(widths, item_df.columns)
+
+    for df_idx, row in item_df.iterrows():
+        datum = [row["Item #"],
+                 row["Customer Part #"],
+                 row["Item Name"],
+                 row["Unit"],
+                 row["New Price"],
+                 row["From Date"]]
+        pdf.create_table_row(widths, datum)
+    
+    pdf.create_table_footer()
+    
+    pdf.output(f'output/{cust_acct}.pdf', 'F')
+
+    #pdf.output('test.pdf', 'F')
+    #print()
+
